33c8ef5c86887ff79c8bf31f4de74281b4e67bdf
Nureldin <nureldin.nasrullah@gmail.com>
2015-03-24 19:59:30 +1100

    working through animation tute (1)
    added 2 new files (chuck spritesheet & sprite.js), including code for
    idle animation & left-right animation, modified vector2.js to be "_x" &
    "_y" instead

--- Platformer Game/player.js
+++ Platformer Game/player.js
@@ -1,24 +1,50 @@
+var LEFT = 0;
+var RIGHT = 0;
+
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_SHOOT_LEFT = 3;
+var ANIM_CLIMB = 4;
+var ANIM_IDLE_RIGHT = 5;
+var ANIM_JUMP_RIGHT = 6;
+var ANIM_WALK_RIGHT = 7;
+var ANIM_SHOOT_RIGHT = 8;
+var ANIM_MAX = 9;
+
 var Player = function() {
-this.image = document.createElement("img");
+this.sprite = new Sprite("ChuckNorris.png");
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [0, 1, 2, 3, 4, 5, 6, 7]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [8, 9, 10, 11, 12]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [52, 53, 54, 55, 56, 56, 57, 58, 59]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [60, 61, 62, 63, 64]);
+this.sprite.buildAnimation(12, 8, 165, 126, 0.05, [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+
+for (var i=0; i < 6; i++)
+{
+	this.sprite.setAnimationOffset(i, -55, -87);
+}
+
 this.position = new Vector2();
 this.position.set(9*TILE, 0*TILE);
 
 this.width = 159;
 this.height = 163;
 
-this.offset = new Vector2();
-this.offset.set(-55,-87);
-
 this.velocity = new Vector2;
 
 this.falling = true;
 this.jumping = false;
 
-this.image.src = "hero.png";
+this.direction = LEFT;
+
 };
 
 Player.prototype.update = function(deltaTime)
 {
+this.sprite.update(deltaTime);
+
 		var left = false;
 		var right = false;
 		var jump = false;
@@ -27,10 +53,16 @@ Player.prototype.update = function(deltaTime)
 		if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true)
 		{
 			left = true;
+			this.direction = LEFT;
+			if(this.sprite.currentAnimation != ANIM_WALK_LEFT)
+				this.sprite.setAnimation(ANIM_WALK_LEFT);
 		}
 		if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true) 
 		{
 			right = true;
+			this.direction = RIGHT;
+			if(this.sprite.currentAnimation != ANIM_WALK_RIGHT)
+				this.sprite.setAnimation(ANIM_WALK_RIGHT);
 		}
 		if(keyboard.isKeyDown(keyboard.KEY_SPACE) == true)
 		{
@@ -127,9 +159,5 @@ var celldiag = cellAtTileCoord(LAYER_PLATFORMS, tx + 1, ty + 1);
 
 Player.prototype.draw = function()
 {
-	context.save();
-		context.translate(this.position.x, this.position.y);
-		context.rotate(this.rotation);
-		context.drawImage(this.image, -this.width/2, -this.height/2);
-context.restore();
+this.sprite.draw(context, this.position.x, this.position.y);
 }
